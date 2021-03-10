6dd3ae1f4694ed770d3c3b7ab5714cdc1bc9b646
calgara12 <michael.polzer@gmx.net>
2021-03-10 23:31:26 +0100

    Add MainActivity

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.einzelbeispiel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import com.example.einzelbeispiel.NetworkTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendButton;
+    EditText matNumberTextBox;
+    TextView serverResponseTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        serverResponseTextView = findViewById(R.id.lblResponse);
+        matNumberTextBox = findViewById(R.id.tbInputNum);
+        sendButton = findViewById(R.id.buSend);
+
+
+
+
+
+        sendButton.setOnClickListener(
+                v -> {
+                    NetworkTask networkTask = new NetworkTask(matNumberTextBox.getText().toString());
+                    networkTask.start();
+                    try{
+                        networkTask.join();
+                    }
+                    catch (Exception ex){
+                        Log.e("Error",ex.getMessage());
+                    }
+
+                    serverResponseTextView.setText(networkTask.getServerResponse());
+                }
+        );
+
     }
+
+
+
+
 }
