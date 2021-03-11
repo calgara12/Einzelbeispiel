18aaac3a1e8e88c417fa39567598b2e5ee34af97
calgara12 <michael.polzer@gmx.net>
2021-03-11 10:19:14 +0100

    Finish Ex2

--- app/src/main/java/com/example/einzelbeispiel/MainActivity.java
+++ app/src/main/java/com/example/einzelbeispiel/MainActivity.java
@@ -18,29 +18,34 @@ import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import com.example.einzelbeispiel.NetworkTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button sendButton;
-    EditText matNumberTextBox;
-    TextView serverResponseTextView;
+    private Button _sendButton;
+    private Button _calcCrossSumButton;
+    private EditText _matNumberTextBox;
+    private TextView _serverResponseTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        serverResponseTextView = findViewById(R.id.lblResponse);
-        matNumberTextBox = findViewById(R.id.tbInputNum);
-        sendButton = findViewById(R.id.buSend);
+        _serverResponseTextView = findViewById(R.id.lblResponse);
+        _matNumberTextBox = findViewById(R.id.tbInputNum);
+        _sendButton = findViewById(R.id.buSend);
+        _calcCrossSumButton = findViewById(R.id.buCrossSum);
 
 
 
 
 
-        sendButton.setOnClickListener(
+        _sendButton.setOnClickListener(
                 v -> {
-                    NetworkTask networkTask = new NetworkTask(matNumberTextBox.getText().toString());
+                    NetworkTask networkTask = new NetworkTask(_matNumberTextBox.getText().toString());
                     networkTask.start();
                     try{
                         networkTask.join();
@@ -49,10 +54,39 @@ public class MainActivity extends AppCompatActivity {
                         Log.e("Error",ex.getMessage());
                     }
 
-                    serverResponseTextView.setText(networkTask.getServerResponse());
+                    _serverResponseTextView.setText(networkTask.getServerResponse());
                 }
         );
 
+        _calcCrossSumButton.setOnClickListener(
+                v -> {
+                    int crossSum = 0;
+                    String matricularNumber = _matNumberTextBox.getText().toString();
+                    for(char c: matricularNumber.toCharArray()){
+                        crossSum += (int) c - '0';
+                    }
+
+                    String binaryValue = "";
+                    ArrayList binaryAL = decimalToBinary(crossSum);
+                    for(Object i: binaryAL){
+                        binaryValue += i.toString();
+                    }
+                    _serverResponseTextView.setText(binaryValue);
+                }
+        );
+
+    }
+
+    ArrayList<Integer> decimalToBinary(int decimalNum){
+        int temp = 0;
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        for(int i = 0; decimalNum > 0;i++){
+            result.add(decimalNum % 2);
+            decimalNum = decimalNum/2;
+        }
+        Collections.reverse(result);
+        return result;
     }
 
 
